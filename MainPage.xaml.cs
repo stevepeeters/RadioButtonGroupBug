502bb1742dbd20193bafb7c44af47bcd95838f96
te <te@L0-OBJT-123>
2022-11-05 12:47:17 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+		Group1Panel.BindingContext = new ViewModel();
+		Group2Panel.BindingContext = new ViewModel();
+	}
+}
+
+public class ViewModel : BindableObject
+{
+	private bool boolValue = true;
+
+	public bool BoolValue
+	{
+		get => boolValue;
+		set 
+		{
+			boolValue = value;
+			OnPropertyChanged();
+		}
+	}
+}
+
